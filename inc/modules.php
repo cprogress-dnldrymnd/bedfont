b03b0958a25355d0740799530b462dab2bfe5bd6
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-02 21:55:15 +0800

    Update modules.php

--- inc/modules.php
+++ inc/modules.php
@@ -154,11 +154,13 @@ function _two_column_image_text($module)
     return "<section class='two-column-image-text'>
 	<div class='container'>
 		<div class='row $image_position'>
-			<div class='$col_1_class'>
+			<div class='$col_1_class $style'>
+	<div class='inner'>
+
 				<h2 class='text-left'>$heading</span></h2>
 				$description
                 $button
-		
+			</div>
 			</div>
 			<div class='$col_2_class'>
 				$image
