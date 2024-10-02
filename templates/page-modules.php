479b1edcab9f58a39dceeffd7d6bc9d70e24482d
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-02 14:29:39 +0800

    Update page-modules.php

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -9,10 +9,10 @@ get_header('');
 $modules = get__post_meta_by_id(get_the_ID(), 'modules');
 foreach ($modules as $key => $module) {
     $type = $module['_type'];
-
     switch ($type) {
         case 'layouts':
             $style = $section['style'];
+            echo $style;
             get_template_part('template-parts/modules/hero', $style);
             break;
     }
