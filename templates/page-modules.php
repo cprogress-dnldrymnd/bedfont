42d5d4204be56b3f5b96d4e2a0314c9f5c66bc79
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-02 17:58:49 +0800

    Update page-modules.php

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -7,4 +7,25 @@ Template Name: --Modules
 
 get_header('');
 the_content();
+?>
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+<script>
+    const config = {
+        type: 'carousel',
+        perView: 3,
+        autoplay: 5000,
+        breakpoints: {
+            1000: {
+                perView: 2
+            },
+            600: {
+                perView: 1
+            }
+        }
+    };
+
+    new Glide('.glide', config).mount()
+</script>
+
+<?php
 get_footer('');
