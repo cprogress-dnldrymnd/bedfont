c0eb6434867e212c5907a9724c3111bad34d0e9a
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-02 22:55:26 +0800

    css

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -355,7 +355,7 @@ Container::make('post_meta', 'Modules')
                                     'dark-blue'      => 'Style Dark Blue',
                                     'dark-green'      => 'Style Dark Green',
                                     'green'      => 'Style Green',
-                                    'yellow'      => 'Style Yello',
+                                    'yellow'      => 'Style Yellow',
                                 )
                             )
                             ->set_conditional_logic(
