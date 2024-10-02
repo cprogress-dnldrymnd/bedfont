23f1f69056b122db0f9ffbf708c9a092f9e6bc87
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-02 14:34:19 +0800

    Update hero-style-1.php

--- template-parts/modules/hero-style-1.php
+++ template-parts/modules/hero-style-1.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-6 my-auto d-block order-2 order-lg-1">
-                <h1><?= __($module['heading'], 'bedfont-custom-fields') ?></h1>
+                <h1><?= __($args['module']['heading'], 'bedfont-custom-fields') ?></h1>
                 <h2><em>World leaders in breath analysis.</em></h2>
                 <div id="line_top"></div>
                 <div id="line_bottom"></div>
