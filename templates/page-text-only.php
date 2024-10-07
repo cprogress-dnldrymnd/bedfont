a049bead7cf44363c546003f05939b1427e8e358
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-07 23:57:10 +0800

    Create page-text-only.php

---
+++ templates/page-text-only.php
@@ -0,0 +1,22 @@
+<?php
+/*
+
+Template Name: --Text Only
+
+ */
+
+get_header('');
+?>
+<section class="subtle-bg">
+    <img decoding="async" src="/wp-content/themes/bedfont/assets/img/subtle-bg.jpg" class="subtle-bg" alt="Image of subtle website background">
+    <div class="container">
+        <div class="row m-0">
+            <div class="col-12">
+                <?php the_content() ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer('');
