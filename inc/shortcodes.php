6df849d480b99918f1b195f5a067e8f4c89442ff
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-10-10 13:02:12 +0800

    Update shortcodes.php

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -103,6 +103,7 @@ function careers()
     $jobs = get_posts(array(
         'post_type' => 'careers',
         'numberposts' => -1,
+        'suppress_filters' => false
     ));
     $html = "<div class='job-holder'>";
     foreach ($jobs as $key => $job) {
