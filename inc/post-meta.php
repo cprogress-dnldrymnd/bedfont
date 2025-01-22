d6ea248f6880c421c35affd18fca19a4ff1d1ec6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-01-22 23:24:25 +0800

    Update post-meta.php

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -44,8 +44,12 @@ Container::make('post_meta', 'Modules')
                 Field::make('complex', 'custom_css', __('Section Custom CSS'))->set_width(100)
                     ->add_fields(array(
                         Field::make('text', 'css_selector', __('Selector'))->set_width(33),
-                        Field::make('text', 'css_property', __('Property'))->set_width(33),
-                        Field::make('text', 'css_value', __('Value'))->set_width(33),
+                        Field::make('complex', 'css_properties', __('Properties'))->set_width(100)
+                            ->add_fields(array(
+                                Field::make('text', 'css_property', __('Property'))->set_width(33),
+                                Field::make('text', 'css_value', __('Value'))->set_width(33),
+                            ))
+                            ->set_header_template('<%- css_selector %> { <%- css_property %> : <%- css_value %> }'),
                     ))
                     ->set_header_template('<%- css_selector %> { <%- css_property %> : <%- css_value %> }'),
                 Field::make('text', 'section_id', __('Section ID'))->set_width(50),
