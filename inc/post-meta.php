10aabedfd39a79ef4eca344eb8799dc627de2e47
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-01-22 23:10:04 +0800

    Update post-meta.php

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -47,7 +47,7 @@ Container::make('post_meta', 'Modules')
                         Field::make('text', 'css_property', __('Property'))->set_width(33),
                         Field::make('text', 'css_value', __('Value'))->set_width(33),
                     ))
-                    ->set_header_template('Hero: <%- css_selector { css_property : css_value } %>'),
+                    ->set_header_template('Hero: <%- css_selector %> { <%- css_selector %> : <%- css_value %> }'),
                 Field::make('text', 'section_id', __('Section ID'))->set_width(50),
                 Field::make('text', 'section_classes', __('Section Classes'))->set_width(50),
                 Field::make('select', 'style', __('Style'))
